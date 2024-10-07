cc710905f9f8098f0a616944d137de814b4b0699
Joseph Lunderville <joe.lunderville@gmail.com>
2024-10-07 02:01:13 -0700

    Experiment with geometry shader

--- src/gltest.cpp
+++ src/gltest.cpp
@@ -1,15 +1,4 @@
-/*
-    Minimal SDL2 + OpenGL3 example.
-    Author: https://github.com/koute
-    This file is in the public domain; you can do whatever you want with it.
-    In case the concept of public domain doesn't exist in your jurisdiction
-    you can also use this code under the terms of Creative Commons CC0 license,
-    either version 1.0 or (at your option) any later version; for details see:
-        http://creativecommons.org/publicdomain/zero/1.0/
-    This software is distributed without any warranty whatsoever.
-    Compile and run with: gcc opengl3_hello.c `sdl2-config --libs --cflags` -lGL
-   -Wall && ./a.out
-*/
+// Based on minimal SDL2 + OpenGL3 example by https://github.com/koute
 
 #define GL_GLEXT_PROTOTYPES
 
@@ -50,22 +39,60 @@ static inline void mat4x4_ortho(t_mat4x4 out, float left, float right,
 #undef T
 }
 
-static const char *vertex_shader =
-    "#version 130\n"
-    "in vec2 i_position;\n"
-    // "in int i_char;\n"
-    "in vec4 i_fg_color;\n"
-    "in vec4 i_bg_color;\n"
+static const char *vertex_shader = "#version 330 core\n"
+                                   "in vec2 i_cell;\n"
+                                   "in uint i_char;\n"
+                                   "in vec4 i_fg_color;\n"
+                                   "in vec4 i_bg_color;\n"
+                                   "out vec2 g_cell;\n"
+                                   "out uint g_char;\n"
+                                   "out vec4 g_fg_color;\n"
+                                   "out vec4 g_bg_color;\n"
+                                   "uniform uint u_blink_control;\n"
+                                   "void main() {\n"
+                                   "    g_cell = i_cell;\n"
+                                   "    g_char = i_char;\n"
+                                   "    g_fg_color = i_fg_color;\n"
+                                   "    g_bg_color = i_bg_color;\n"
+                                   "}\n";
+
+static const char *geometry_shader =
+    "#version 330 core\n"
+    "layout(points) in;\n"
+    "layout(triangle_strip, max_vertices = 4) out;\n"
+    "in uint g_char[];\n"
+    "in vec2 g_cell[];\n"
+    "in vec4 g_fg_color[];\n"
+    "in vec4 g_bg_color[];\n"
     "out vec4 v_fg_color;\n"
     "out vec4 v_bg_color;\n"
+    "uniform vec2 u_cell_size;\n"
     "uniform mat4 u_projection_matrix;\n"
     "void main() {\n"
-    "    v_fg_color = i_fg_color;\n"
-    "    v_bg_color = i_bg_color;\n"
-    "    gl_Position = u_projection_matrix * vec4(i_position, 0, 1.0);\n"
+    "    v_fg_color = g_fg_color[0];\n"
+    "    v_bg_color = g_bg_color[0];\n"
+    "    gl_Position = u_projection_matrix * vec4(g_cell[0].x * u_cell_size.x, "
+    "g_cell[0].y * u_cell_size.y, 0, 1.0);\n"
+    "    EmitVertex();\n"
+    "    v_fg_color = g_bg_color[0];\n"
+    "    v_bg_color = g_fg_color[0];\n"
+    "    gl_Position = u_projection_matrix * vec4((g_cell[0].x + 1) * "
+    "u_cell_size.x, g_cell[0].y * u_cell_size.y, 0, 1.0);\n"
+    "    EmitVertex();\n"
+    "    v_fg_color = g_bg_color[0];\n"
+    "    v_bg_color = g_fg_color[0];\n"
+    "    gl_Position = u_projection_matrix * vec4(g_cell[0].x * u_cell_size.x, "
+    "(g_cell[0].y + 1) * u_cell_size.y, 0, 1.0);\n"
+    "    EmitVertex();\n"
+    "    v_fg_color = g_fg_color[0];\n"
+    "    v_bg_color = g_bg_color[0];\n"
+    "    gl_Position = u_projection_matrix * vec4((g_cell[0].x + 1) * "
+    "u_cell_size.x, (g_cell[0].y + 1) * u_cell_size.y, 0, 1.0);\n"
+    "    EmitVertex();\n"
+    "    EndPrimitive();\n"
     "}\n";
 
-static const char *fragment_shader = "#version 130\n"
+static const char *fragment_shader = "#version 330 core\n"
                                      "in vec4 v_fg_color;\n"
                                      "in vec4 v_bg_color;\n"
                                      "out vec4 o_color;\n"
@@ -74,11 +101,53 @@ static const char *fragment_shader = "#version 130\n"
                                      "}\n";
 
 typedef enum t_attrib_id {
-  attrib_position,
+  attrib_cell,
   attrib_char,
   attrib_fg_color,
   attrib_bg_color
 } t_attrib_id;
+
+GLuint compile_shader(GLenum shader_type, GLchar const *shader_source) {
+  char const *shader_type_name = "<unknown>";
+
+  if (shader_type == GL_VERTEX_SHADER) {
+    shader_type_name = "vertex";
+  } else if (shader_type == GL_GEOMETRY_SHADER) {
+    shader_type_name = "geometry";
+  } else if (shader_type == GL_FRAGMENT_SHADER) {
+    shader_type_name = "fragment";
+  }
+
+  GLuint shader = glCreateShader(shader_type);
+
+  if (shader == 0) {
+    return 0;
+  }
+
+  GLint length = strlen(geometry_shader);
+  glShaderSource(shader, 1, &shader_source, &length);
+  glCompileShader(shader);
+
+  GLint status;
+  glGetShaderiv(shader, GL_COMPILE_STATUS, &status);
+  if (status == GL_FALSE) {
+    GLint log_length = 0;
+    glGetShaderiv(shader, GL_INFO_LOG_LENGTH, &log_length);
+    if (log_length > 0) {
+      char *log = (char *)malloc(log_length);
+      glGetShaderInfoLog(shader, log_length, NULL, log);
+      fprintf(stderr, "%s shader compilation failed:\n%s\n", shader_type_name,
+              log);
+      free(log);
+    } else {
+      fprintf(stderr, "%s shader compilation failed (not able to get log)\n",
+              shader_type_name);
+    }
+    return 0;
+  }
+
+  return shader;
+}
 
 int main(int argc, char **argv) {
   (void)argc;
@@ -111,34 +180,17 @@ int main(int argc, char **argv) {
       window_height, SDL_WINDOW_OPENGL | SDL_WINDOW_SHOWN);
   SDL_GLContext context = SDL_GL_CreateContext(window);
 
-  GLuint vs, fs, program;
+  GLuint vs = compile_shader(GL_VERTEX_SHADER, vertex_shader);
+  GLuint gs = compile_shader(GL_GEOMETRY_SHADER, geometry_shader);
+  GLuint fs = compile_shader(GL_FRAGMENT_SHADER, fragment_shader);
 
-  vs = glCreateShader(GL_VERTEX_SHADER);
-  fs = glCreateShader(GL_FRAGMENT_SHADER);
-
-  int length = strlen(vertex_shader);
-  glShaderSource(vs, 1, (const GLchar **)&vertex_shader, &length);
-  glCompileShader(vs);
-
-  GLint status;
-  glGetShaderiv(vs, GL_COMPILE_STATUS, &status);
-  if (status == GL_FALSE) {
-    fprintf(stderr, "vertex shader compilation failed\n");
-    return 1;
+  if (vs == 0 || gs == 0 || fs == 0) {
+    return 0;
   }
 
-  length = strlen(fragment_shader);
-  glShaderSource(fs, 1, (const GLchar **)&fragment_shader, &length);
-  glCompileShader(fs);
-
-  glGetShaderiv(fs, GL_COMPILE_STATUS, &status);
-  if (status == GL_FALSE) {
-    fprintf(stderr, "fragment shader compilation failed\n");
-    return 1;
-  }
-
-  program = glCreateProgram();
+  GLuint program = glCreateProgram();
   glAttachShader(program, vs);
+  glAttachShader(program, gs);
   glAttachShader(program, fs);
 
   glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_S, GL_CLAMP_TO_EDGE);
@@ -164,11 +216,27 @@ int main(int argc, char **argv) {
                GL_UNSIGNED_BYTE, data);
   // glGenerateMipmap(GL_TEXTURE_2D);
 
-  glBindAttribLocation(program, attrib_position, "i_position");
-  // glBindAttribLocation(program, attrib_char, "i_char");
+  glBindAttribLocation(program, attrib_cell, "i_cell");
+  glBindAttribLocation(program, attrib_char, "i_char");
   glBindAttribLocation(program, attrib_fg_color, "i_fg_color");
   glBindAttribLocation(program, attrib_bg_color, "i_bg_color");
   glLinkProgram(program);
+
+  GLint status;
+  glGetProgramiv(program, GL_LINK_STATUS, &status);
+  if (status == GL_FALSE) {
+    GLint log_length = 0;
+    glGetProgramiv(program, GL_INFO_LOG_LENGTH, &log_length);
+    if (log_length > 0) {
+      char *log = (char *)malloc(log_length);
+      glGetProgramInfoLog(program, log_length, NULL, log);
+      fprintf(stderr, "shader program link failed:\n%s\n", log);
+      free(log);
+    } else {
+      fprintf(stderr, "shader program link failed (not able to get log)\n");
+    }
+    return 0;
+  }
 
   glUseProgram(program);
 
@@ -188,14 +256,14 @@ int main(int argc, char **argv) {
 
   glBindBuffer(GL_ARRAY_BUFFER, rasterize_structure_vbo);
 
-  glEnableVertexAttribArray(attrib_position);
+  glEnableVertexAttribArray(attrib_cell);
   glEnableVertexAttribArray(attrib_fg_color);
   glEnableVertexAttribArray(attrib_bg_color);
 
   struct rasterize_structure_t {
     GLfloat fg_color[4];
     GLfloat bg_color[4];
-    GLfloat position[2];
+    GLfloat cell[2];
   };
 
   rasterize_structure_t
@@ -203,42 +271,17 @@ int main(int argc, char **argv) {
 
   for (size_t i = 0; i < height_chars; ++i) {
     for (size_t j = 0; j < width_chars; ++j) {
-      for (size_t k = 0; k < 6; ++k) {
-        size_t n = (i * width_chars + j) * 6 + k;
-        g_rasterize_structure_data[n].fg_color[0] = k > 0;
-        g_rasterize_structure_data[n].fg_color[1] = k > 1;
-        g_rasterize_structure_data[n].fg_color[2] = k > 2;
-        g_rasterize_structure_data[n].fg_color[3] = 1.0f;
-        g_rasterize_structure_data[n].bg_color[0] = 0.0f;
-        g_rasterize_structure_data[n].bg_color[1] = 0.0f;
-        g_rasterize_structure_data[n].bg_color[2] = 0.0f;
-        g_rasterize_structure_data[n].bg_color[3] = 1.0f;
-      }
-      size_t n = (i * width_chars + j) * 6;
-      g_rasterize_structure_data[n + 0].position[0] = j * char_cell_width_px;
-      g_rasterize_structure_data[n + 0].position[1] = i * char_cell_height_px;
-      g_rasterize_structure_data[n + 1].position[0] =
-          (j + 1) * char_cell_width_px;
-      g_rasterize_structure_data[n + 1].position[1] =
-          (i + 0) * char_cell_height_px;
-      g_rasterize_structure_data[n + 2].position[0] =
-          (j + 1) * char_cell_width_px;
-      g_rasterize_structure_data[n + 2].position[1] =
-          (i + 1) * char_cell_height_px;
-      g_rasterize_structure_data[n + 3].position[0] = j * char_cell_width_px;
-      g_rasterize_structure_data[n + 3].position[1] = i * char_cell_height_px;
-      g_rasterize_structure_data[n + 4].position[0] =
-          (j + 1) * char_cell_width_px;
-      g_rasterize_structure_data[n + 4].position[1] =
-          (i + 1) * char_cell_height_px;
-      g_rasterize_structure_data[n + 5].position[0] =
-          (j + 0) * char_cell_width_px;
-      g_rasterize_structure_data[n + 5].position[1] =
-          (i + 1) * char_cell_height_px;
-      // g_rasterize_structure_data[n + 3].position[0] =
-      //     (j + 0) * char_cell_width_px;
-      // g_rasterize_structure_data[n + 3].position[1] =
-      //     (i + 1) * char_cell_height_px;
+      size_t n = i * width_chars + j;
+      g_rasterize_structure_data[n].fg_color[0] = 1.0f;
+      g_rasterize_structure_data[n].fg_color[1] = 1.0f;
+      g_rasterize_structure_data[n].fg_color[2] = 1.0f;
+      g_rasterize_structure_data[n].fg_color[3] = 1.0f;
+      g_rasterize_structure_data[n].bg_color[0] = 0.0f;
+      g_rasterize_structure_data[n].bg_color[1] = 0.0f;
+      g_rasterize_structure_data[n].bg_color[2] = 0.0f;
+      g_rasterize_structure_data[n].bg_color[3] = 1.0f;
+      g_rasterize_structure_data[n].cell[0] = j;
+      g_rasterize_structure_data[n].cell[1] = i;
     }
   }
 
@@ -248,36 +291,39 @@ int main(int argc, char **argv) {
   glVertexAttribPointer(attrib_bg_color, 4, GL_FLOAT, GL_FALSE,
                         sizeof(rasterize_structure_t),
                         (void *)(offsetof(rasterize_structure_t, bg_color)));
-  glVertexAttribPointer(attrib_position, 2, GL_FLOAT, GL_FALSE,
+  glVertexAttribPointer(attrib_cell, 2, GL_FLOAT, GL_FALSE,
                         sizeof(rasterize_structure_t),
-                        (void *)(offsetof(rasterize_structure_t, position)));
+                        (void *)(offsetof(rasterize_structure_t, cell)));
 
   glBufferData(GL_ARRAY_BUFFER, sizeof(g_rasterize_structure_data),
                g_rasterize_structure_data, GL_STATIC_DRAW);
 
-  // glBindBuffer(GL_ARRAY_BUFFER, rasterize_contents_vbo);
+  glBindBuffer(GL_ARRAY_BUFFER, rasterize_contents_vbo);
 
-  // GLubyte g_rasterize_contents_data[width_chars * height_chars * 4][1];
-  // for (size_t i = 0; i < height_chars; ++i) {
-  //   for (size_t j = 0; j < width_chars; ++j) {
-  //     for (size_t k = 0; k < 4; ++k) {
-  //       size_t n = (i * width_chars + j) * 4 + k;
-  //       g_rasterize_contents_data[n][0] = (GLubyte)n;
-  //     }
-  //   }
-  // }
+  GLubyte g_rasterize_contents_data[width_chars * height_chars * 6][1];
+  for (size_t i = 0; i < height_chars; ++i) {
+    for (size_t j = 0; j < width_chars; ++j) {
+      size_t n = i * width_chars + j;
+      g_rasterize_contents_data[n][0] = (GLubyte)n;
+    }
+  }
 
-  // glEnableVertexAttribArray(attrib_char);
+  glEnableVertexAttribArray(attrib_char);
 
-  // glVertexAttribPointer(attrib_char, 1, GL_UNSIGNED_BYTE, GL_FALSE,
-  //                       sizeof(*g_rasterize_contents_data), (void *)(0));
+  glVertexAttribPointer(attrib_char, 1, GL_UNSIGNED_BYTE, GL_FALSE,
+                        sizeof(*g_rasterize_contents_data), (void *)(0));
 
-  // glBufferData(GL_ARRAY_BUFFER, sizeof(g_rasterize_contents_data),
-  //              g_rasterize_contents_data, GL_STATIC_DRAW);
+  glBufferData(GL_ARRAY_BUFFER, sizeof(g_rasterize_contents_data),
+               g_rasterize_contents_data, GL_STATIC_DRAW);
+
+  glUniform1ui(glGetUniformLocation(program, "u_blink_control"), 1);
+
+  glUniform2f(glGetUniformLocation(program, "u_cell_size"), char_cell_width_px,
+              char_cell_height_px);
 
   t_mat4x4 projection_matrix;
   mat4x4_ortho(projection_matrix, 0.0f, (float)width_px, (float)height_px, 0.0f,
-               0.0f, 1.0f);
+               0.0f, 256.0f);
   glUniformMatrix4fv(glGetUniformLocation(program, "u_projection_matrix"), 1,
                      GL_FALSE, projection_matrix);
 
@@ -286,7 +332,7 @@ int main(int argc, char **argv) {
     glClear(GL_COLOR_BUFFER_BIT);
 
     glBindVertexArray(rasterize_vao);
-    glDrawArrays(GL_TRIANGLES, 0,  width_chars * height_chars * 6);
+    glDrawArrays(GL_POINTS, 0, width_chars * height_chars);
 
     SDL_GL_SwapWindow(window);
     SDL_Delay(1);
